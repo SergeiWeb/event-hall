121828db7d19be240f089cdb3271ca799d95e035
Sergei <sergokondrashevskij@ukr.net>
2021-02-14 17:16:36 +0200

    wrote the functionality of the scroll sliders

--- scripts/home.js
+++ scripts/home.js
@@ -4,20 +4,223 @@ const navbar = document.querySelector('#navbar')
 
 const homeNext = document.querySelector('.home__next')
 
-let tooltipArr = []
+let is_allow_scroll = true
+let isScroll = true
 
-for (const item of homeItems) {
-	tooltipArr.push(item.getAttribute('data-tooltip'))
+const sliderContent = $('.slider-content')
+	.slick({
+		vertical: true,
+		verticalSwiping: true,
+		infinite: false,
+		draggable: false,
+		swipe: true,
+		dots: true,
+		appendDots: $('.fp-bullets-slider'),
+		customPaging: function (slider, i) {
+			const dataTooltip = $(slider.$slides[i]).data('tooltip')
+			return `
+				<span class="fp-bullets-dot"></span>
+				<span class="fp-bullets-tooltip">${dataTooltip}</span>
+			`
+		},
+		slidesToShow: 1,
+		prevArrow: '.our__prev',
+		nextArrow: '.our__next',
+		cssEase: 'ease',
+		speed: 500,
+	})
+	.on('wheel', function (event) {
+		if (is_allow_scroll) {
+			is_allow_scroll = false
+
+			setTimeout(function () {
+				is_allow_scroll = true
+			}, 500)
+
+			if (event.originalEvent.deltaY > 0) {
+				if (
+					!(
+						$(this).slick('slickCurrentSlide') ==
+						$(this).find('.section-slide').length - 1
+					)
+				) {
+					event.preventDefault()
+					$(this).slick('slickNext')
+				} else {
+					fullpage_api.setAllowScrolling(true, 'down')
+					isScroll = true
+					return
+				}
+
+				if ($(this).slick('slickCurrentSlide') == 0) {
+					changeActiveClass('bullets-slide-0')
+				}
+
+				if ($(this).slick('slickCurrentSlide') == 1) {
+					changeActiveClass('bullets-slide-1')
+				}
+
+				if (
+					$(this).slick('slickCurrentSlide') ==
+					$(this).find('.section-slide').length - 1
+				) {
+					changeActiveClass('bullets-slide-2')
+				}
+			} else {
+				if ($(this).slick('slickCurrentSlide') == 0) {
+					fullpage_api.setAllowScrolling(true, 'up')
+					// fullpage_api.setAllowScrolling(false, 'down')
+
+					changeActiveClass('bullets-slide-0')
+					return
+				}
+
+				if ($(this).slick('slickCurrentSlide') == 1) {
+					fullpage_api.setAllowScrolling(false)
+
+					changeActiveClass('bullets-slide-1')
+				}
+
+				if (
+					$(this).slick('slickCurrentSlide') ==
+					$(this).find('.section-slide').length - 1
+				) {
+					changeActiveClass('bullets-slide-2')
+				}
+
+				event.preventDefault()
+				$(this).slick('slickPrev')
+			}
+		}
+	})
+	.on('afterChange', function (event, slick, currentSlide, nextSlide) {
+		for (const item of slick.$slides) {
+			if (item.classList.contains('slick-current')) {
+				document.querySelector('.home__block').style.backgroundImage =
+					`url('${item.getAttribute('data-bg')}')` || '#313643'
+			}
+		}
+
+		if (slick.$slides[0].classList.contains('slick-active') && !isScroll) {
+			fullpage_api.setAllowScrolling(true, 'up')
+			isScroll = true
+
+			changeActiveClass('bullets-slide-0')
+		}
+
+		if (slick.$slides[1].classList.contains('slick-active')) {
+			fullpage_api.setAllowScrolling(false)
+			isScroll = false
+
+			changeActiveClass('bullets-slide-1')
+		}
+
+		if (
+			slick.$slides[slick.$slides.length - 1].classList.contains('slick-active')
+		) {
+			fullpage_api.setAllowScrolling(true, 'down')
+			isScroll = true
+
+			changeActiveClass('bullets-slide-2')
+		}
+	})
+	.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+		for (const item of slick.$slides) {
+			if (item.classList.contains('slick-current')) {
+				document.querySelector('.home__block').style.backgroundImage =
+					`url('${item.getAttribute('data-bg')}')` || '#313643'
+			}
+		}
+
+		if (slick.$slides[0].classList.contains('slick-active')) {
+			fullpage_api.setAllowScrolling(true, 'up')
+			isScroll = true
+
+			changeActiveClass('bullets-slide-0')
+		}
+
+		if (slick.$slides[1].classList.contains('slick-active')) {
+			fullpage_api.setAllowScrolling(false)
+			isScroll = false
+		}
+
+		if (
+			slick.$slides[slick.$slides.length - 1].classList.contains('slick-active')
+		) {
+			fullpage_api.setAllowScrolling(true, 'down')
+			isScroll = true
+		}
+	})
+
+$('.fp-bullets-slider .slick-dots li').addClass('fp-bullets-item')
+
+for (let i = 0; i < $('.fp-bullets-slider .slick-dots li').length; i++) {
+	$('.fp-bullets-slider .slick-dots li')[i].classList.add(`bullets-slide-${i}`)
 }
 
-const myFullpage = new fullpage('#fullpage', {
+const fpBulletsItem = document.querySelectorAll('.fp-bullets-item')
+
+for (let i = 0; i < fpBulletsItem.length; i++) {
+	const elem = fpBulletsItem[i]
+
+	elem.addEventListener('click', event => {
+		for (let k = 0; k < fpBulletsItem.length; k++) {
+			fpBulletsItem[k].classList.remove('active')
+		}
+
+		if (elem.classList.contains('bullets-home')) {
+			elem.classList.add('active')
+			fullpage_api.moveTo(1, 0)
+			$('.slider-content').slick('slickGoTo', sliderContent[0].slick.$slides[0])
+		}
+
+		if (elem.classList.contains('bullets-slide-0')) {
+			elem.classList.add('active')
+			fullpage_api.moveTo(2, 0)
+			$('.slider-content').slick('slickGoTo', sliderContent[0].slick.$slides[0])
+		}
+
+		if (elem.classList.contains('bullets-slide-1')) {
+			elem.classList.add('active')
+			fullpage_api.moveTo(2, 0)
+			$('.slider-content').slick('slickGoTo', sliderContent[0].slick.$slides[1])
+		}
+
+		if (elem.classList.contains('bullets-slide-2')) {
+			elem.classList.add('active')
+			fullpage_api.moveTo(2, 0)
+			$('.slider-content').slick(
+				'slickGoTo',
+				sliderContent[0].slick.$slides.length - 1
+			)
+		}
+
+		if (elem.classList.contains('bullets-contacts')) {
+			elem.classList.add('active')
+			fullpage_api.moveTo(3, 0)
+			$('.slider-content').slick(
+				'slickGoTo',
+				sliderContent[0].slick.$slides.length - 1
+			)
+		}
+	})
+}
+
+function changeActiveClass(clasName = '') {
+	for (let i = 0; i < fpBulletsItem.length; i++) {
+		fpBulletsItem[i].classList.remove('active')
+
+		if (fpBulletsItem[i].classList.contains(clasName)) {
+			fpBulletsItem[i].classList.add('active')
+		}
+	}
+}
+
+const fpSlider = new fullpage('#fullpage', {
 	continuousVertical: false,
-	navigation: true,
+	navigation: false,
 	sectionSelector: '.home__item',
-	navigationPosition: 'left',
-	showActiveTooltip: true,
 	css3: true,
-	navigationTooltips: tooltipArr,
 	afterLoad(anchorLink, index) {
 		document.querySelector('.who').classList.contains('active')
 			? document.querySelector('#aboutLink').classList.add('active')
@@ -27,18 +230,69 @@ const myFullpage = new fullpage('#fullpage', {
 			? document.querySelector('#contactsLink').classList.add('active')
 			: document.querySelector('#contactsLink').classList.remove('active')
 
+
+		if ((index.isFirst && isScroll) || (index.index == 0 && isScroll)) {
+			fullpage_api.setAllowScrolling(true)
+			setInterval(() => (isScroll = true), 1500)
+
+			changeActiveClass('bullets-home')
+		}
+
 		if (index.isFirst) {
+			changeActiveClass('bullets-home')
 			fullpage_api.setAllowScrolling(true)
 		}
 
-		if (index.index == 1) {
-			fullpage_api.setAllowScrolling(true)
+		if (index.index == 1 || !isScroll) {
+			fullpage_api.setAllowScrolling(true, 'up')
 			fullpage_api.setAllowScrolling(false, 'down')
+			isScroll = false
+
+			if (
+				sliderContent[0].slick.$slides[0].classList.contains('slick-active')
+			) {
+				changeActiveClass('bullets-slide-0')
+			} else if (
+				sliderContent[0].slick.$slides[
+					sliderContent[0].slick.$slides.length - 1
+				].classList.contains('slick-active')
+			) {
+				changeActiveClass('bullets-slide-2')
+			}
 		} else {
 			fullpage_api.setAllowScrolling(true)
+			isScroll = true
 		}
 
-		if (index.isLast) {
+		if ((index.isLast && isScroll) || (index.index == 2 && isScroll)) {
+			isScroll = true
+			fullpage_api.setAllowScrolling(true, 'up')
+			homeNext.style.transform = 'scale(0)'
+			setInterval(() => (isScroll = true), 1500)
+
+			changeActiveClass('bullets-contacts')
+		} else {
+			fullpage_api.setAllowScrolling(false, 'up')
+			homeNext.style.transform = 'none'
+		}
+	},
+	onLeave(index, nextIndex, direction) {
+		if (nextIndex.isFirst && nextIndex.index == 0 && isScroll) {
+			fullpage_api.setAllowScrolling(true)
+			isScroll = true
+		}
+
+		if (nextIndex.index == 1) {
+			fullpage_api.setAllowScrolling(true, 'up')
+			fullpage_api.setAllowScrolling(false, 'down')
+			isScroll = false
+		} else {
+			fullpage_api.setAllowScrolling(true)
+			isScroll = true
+		}
+
+		if (nextIndex.isLast && nextIndex.index == 2 && isScroll) {
+			isScroll = true
 			fullpage_api.setAllowScrolling(true, 'up')
 			homeNext.style.transform = 'scale(0)'
 		} else {
@@ -47,6 +301,9 @@ const myFullpage = new fullpage('#fullpage', {
 		}
 	},
 })
+
+// console.log(fullpage_api.getActiveSection())
+// console.log(fullpage_api.getActiveSlide())
 
 $(document).on('click', '.home__next', function () {
 	fullpage_api.moveSectionDown()
@@ -86,97 +343,6 @@ $('.slider-wrapper').slick({
 	nextArrow: '.who__slider-next',
 	infinite: true,
 })
-
-let is_allow_scroll = true
-
-$('.slider-content')
-	.slick({
-		vertical: true,
-		verticalSwiping: true,
-		infinite: false,
-		draggable: false,
-		swipe: true,
-		slidesToShow: 1,
-		prevArrow: '.our__prev',
-		nextArrow: '.our__next',
-		cssEase: 'linear',
-		speed: 500,
-	})
-	.on('wheel', function (event) {
-		// console.log($(this).find('.section-slide').length - 1)
-
-		if (is_allow_scroll) {
-			is_allow_scroll = false
-			setTimeout(function () {
-				is_allow_scroll = true
-			}, 500)
-
-			if (event.originalEvent.deltaY > 0) {
-				if (
-					!(
-						$(this).slick('slickCurrentSlide') ==
-						$(this).find('.section-slide').length - 1
-					)
-				) {
-					event.preventDefault()
-					$(this).slick('slickNext')
-				} else {
-					fullpage_api.setAllowScrolling(false, 'up')
-					fullpage_api.setAllowScrolling(true, 'down')
-					return
-				}
-			} else {
-				if ($(this).slick('slickCurrentSlide') == 0) {
-					fullpage_api.setAllowScrolling(true, 'up')
-					fullpage_api.setAllowScrolling(false, 'down')
-					return
-				}
-
-				if ($(this).slick('slickCurrentSlide') == 1) {
-					fullpage_api.setAllowScrolling(false)
-				}
-
-				event.preventDefault()
-				$(this).slick('slickPrev')
-			}
-		}
-	})
-	.on('afterChange', function (event, slick, currentSlide, nextSlide) {
-		for (const item of slick.$slides) {
-			if (item.classList.contains('slick-current')) {
-				document.querySelector('.home__block').style.backgroundImage =
-					`url('${item.getAttribute('data-bg')}')` || '#313643'
-			}
-		}
-
-		if (slick.$slides[0].classList.contains('slick-active')) {
-			fullpage_api.setAllowScrolling(true, 'up')
-		}
-
-		if (slick.$slides[1].classList.contains('slick-active')) {
-			fullpage_api.setAllowScrolling(false)
-		}
-
-		if (
-			slick.$slides[slick.$slides.length - 1].classList.contains('slick-active')
-		) {
-			fullpage_api.setAllowScrolling(false, 'up')
-			fullpage_api.setAllowScrolling(true, 'down')
-		}
-	})
-	.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-		for (const item of slick.$slides) {
-			if (item.classList.contains('slick-current')) {
-				document.querySelector('.home__block').style.backgroundImage =
-					`url('${item.getAttribute('data-bg')}')` || '#313643'
-			}
-		}
-
-		if (slick.$slides[0].classList.contains('slick-active')) {
-			fullpage_api.setAllowScrolling(true, 'up')
-			fullpage_api.setAllowScrolling(false, 'down')
-		}
-	})
 
 function init() {
 	const coordinates = { lat: 31.974034869073954, lng: 34.801772305921595 }
